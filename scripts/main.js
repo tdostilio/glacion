a78c0e27ace292afcb807f807b2a925213b80320
tdostilio <tdostilio@gmail.com>
2017-07-27 15:00:02 -0400

    added job posting age parameter to search function

--- scripts/main.js
+++ scripts/main.js
@@ -3,25 +3,26 @@ DICE_BASE_URL = "http://service.dice.com/api/rest/jobsearch/v1/simple.json?"
 // EXAMPLE SEARCH FOR DICE
 // http://service.dice.com/api/rest/jobsearch/v1/simple.json?text=java&city=New+York,+NY search for java jobs in the New York, NY area
 
-function getServerData(criteria,zipcode) {
+function getServerData(criteria,zipcode,date) {
     var locationSearch;
     var textSearch;
     var searchResults = [];
     var jobCount;
+    var postedBy;
         if (criteria) {
             textSearch = "text="+criteria+"&";
         } else {
             textSearch = "";
         }
-        
         if (zipcode) {
             locationSearch = "city="+zipcode;
         } else {
             locationSearch = ""
         }
 
-    console.log(DICE_BASE_URL+textSearch+locationSearch);
-    return $.get(DICE_BASE_URL+textSearch+locationSearch)
+
+    console.log(DICE_BASE_URL+textSearch+locationSearch+"&age="+date);
+    return $.get(DICE_BASE_URL+textSearch+locationSearch+"&age="+date)
         .then(function (data) {
             jobCount = data.count;
             console.log(jobCount);
@@ -53,7 +54,7 @@ function getCompanyName(array) {
 }
 
 // Main Search Function
-getServerData("Java",30342)
+getServerData("Java",30342,1)
     .then(function(data) {
        return getDataArray(data)})
             .then(function(data) {

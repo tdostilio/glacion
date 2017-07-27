5040ad4fde8816e8bd99d603a87f2186bb7f4498
Maxwellgit  Irvine <max.irvine99@gmail.com>
2017-07-27 12:04:43 -0400

    add granim.min.js to scripts and removes unneccesary files

--- granim.js-1.0.6/lib/onResize.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = function() {
-	this.getDimensions();
-	this.canvas.setAttribute('width', this.x1);
-	this.canvas.setAttribute('height', this.y1);
-	this.refreshColors();
-};

3ee7ebfb2178ea3fe1ac709dacaec53219417827
Maxwellgit  Irvine <max.irvine99@gmail.com>
2017-07-27 17:23:39 -0400

    removes animsition

--- scripts/animsition.js
+++ scripts/animsition.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
     loadingClass: 'animsition-loading',
     loadingInner: '', // e.g '<img src="loading.svg" />'
     timeout: false,
-    timeoutCountdown: 5000,
+    timeoutCountdown: 1,
     onLoadEvent: true,
     browser: [ 'animation-duration', '-webkit-animation-duration'],
     // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.

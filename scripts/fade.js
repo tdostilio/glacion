ed2379688a6480adf4f9b5d2161b3c9bc04e5ad9
Maxwellgit  Irvine <max.irvine99@gmail.com>
2017-07-28 09:25:11 -0400

    adds comments for clarity

--- scripts/fade.js
+++ scripts/fade.js
@@ -1,8 +1,10 @@
 var $button = $(".search-button");
 var $container = $(".body-container");
 
+// DOES SOMETHING ON BUTTON CLICK
 $button.on("click", function(event){
     event.preventDefault();
+    // FADES OUT AND CALLS MAP.HTML 
     $container.fadeOut("slow", function(){
         window.location.replace("map.html");
     });

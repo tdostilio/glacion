8dc1b653f01d212e7b958225de3fe9e9c0a8b763
Maxwellgit  Irvine <max.irvine99@gmail.com>
2017-07-28 10:37:18 -0400

    adjusts default map location to be the atlanta tech village

--- scripts/map.js
+++ scripts/map.js
@@ -1,7 +1,7 @@
 function initMap() {
-    var uluru = {lat: -25.363, lng: 131.044};
+    var uluru = {lat: 33.848795, lng: -84.373567};
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
+        zoom: 10,
         center: uluru
     });
     var marker = new google.maps.Marker({

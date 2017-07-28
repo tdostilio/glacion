e591c1adeee91eac10440318b8467b99db17a718
Maxwellgit  Irvine <max.irvine99@gmail.com>
2017-07-28 09:23:50 -0400

    removes all fade classes and fixes JavaScript fade

--- scripts/fade.js
+++ scripts/fade.js
@@ -2,6 +2,8 @@ var $button = $(".search-button");
 var $container = $(".body-container");
 
 $button.on("click", function(event){
-    $container.fadeOut("slow");
-    window.location.replace("map.html");
+    event.preventDefault();
+    $container.fadeOut("slow", function(){
+        window.location.replace("map.html");
+    });
 })

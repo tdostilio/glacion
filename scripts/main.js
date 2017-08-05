d72f541f2b46fe6969a15362456008abbab27b71
tdostilio <tdostilio@gmail.com>
2017-08-05 14:57:40 -0400

    fixed accessible api keys

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-var GOOGLE_MAPS_API = "AIzaSyDyP-CDcYsQMrECY9Nh_EyqNHTiPoCXxKw"; //Tom's Key: AIzaSyDiqCd6BJiVSW2HnxSSEFfqhCboUrToFPw
+var GOOGLE_MAPS_API = config.Max_key;
 var DICE_BASE_URL = "http://service.dice.com/api/rest/jobsearch/v1/simple.json?";
 var GMAPS_URL = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=";
 var $LOCATION = $(".search-location");
@@ -98,12 +98,7 @@ function getCompanyName(array) {
     });
 }
 
-
 var localTestData = ["Chik Fil A", "Accenture Technology Solutions", "Visionaire Partners", "Kavi Software", "Datamatics Global Services Ltd.", "Principle Solutions Group", "Visionaire Partners", "Visionaire Partners", "Genesis10", "Spectrum IT Global INC", "Pyramid Consulting, Inc.", "Arthur Lawrence", "Amzur Technologies, Inc.", "Visionaire Partners", "Brooksource", "Collabera", "Principle Solutions Group", "Visionaire Partners", "Principle Solutions Group", "Principle Solutions Group", "Visionaire Partners", "Visionaire Partners", "Prestige Staffing", "Digital Intelligence Systems, LLC", "Data Resource Technologies", "Principle Solutions Group", "Data Resource Technologies", "Veredus", "Visionaire Partners", "Principle Solutions Group", "Agile Global Solutions, Inc", "Principle Solutions Group", "Visionaire Partners", "Signature Consultants", "Pegasys Systems & Technologies Inc", "Rivi Consulting Group", "Visionaire Partners", "UST Global Inc", "ICON Technologies", "Principle Solutions Group", "Rivi Consulting Group", "Pyramid Consulting, Inc.", "Visionaire Partners", "Datanomics", "Cambridge Technical", "Kforce Inc.", "iStaff", "Travelport", "iStaff", "Swinsoft", "Metasys Technologies", "HMG America", "AptoNet Inc", "Career Guidant, Inc", "Spark Data Solutions, Inc.", "Travelport", "Mindtree Limited", "Alliance Sourcing Network", "firstPRO, Inc.", "firstPRO, Inc.", "Primus Software Corp", "AptoNet Inc", "Career Guidant, Inc", "Paramount Software Solutions, Inc", "Allied Informatics, Inc", "Travelport", "Thompson Technologies", "Robert Half Technology", "Softpros, Inc.", "Mindtree Limited", "Charter Global, Inc.", "iStaff", "iStaff", "Capgemini", "Strivector", "The Judge Group", "Routematch", "M9 Solutions", "3Ci", "Hired by Matrix, Inc.", "Stellar Consulting Solutions", "Primus Software Corp", "Routematch", "Primus Software Corp", "Paramount Software Solutions, Inc", "Metasys Technologies", "iStaff", "Cambridge Technical", "Matrix Resources", "Matrix Resources", "Squires Group, Inc", "Cambridge Technical", "ProActive Resources, Inc", "Techno-Comp, Inc.", "Open Systems Inc", "Professional Insight", "Citadel Information Services Inc", "3i People, Inc.", "Professional Insight", "Travelport", "Paramount Software Solutions, Inc"];
-
-// console.log(localTestData[42]);
-// EXAMPLE GMAPS URL
-// https://maps.googleapis.com/maps/api/place/textsearch/json?query=123+main+street&key=YOUR_API_KEY
 
 var TestString = localTestData[0];
 // console.log(TestString)
@@ -119,7 +114,6 @@ function convertZiptoCity(zipcode) {
 //     .then (function(data) {
 //         return (data);
 //     })
-
 
 function getCoordinates(array, zipcode){
     return convertZiptoCity(zipcode)
